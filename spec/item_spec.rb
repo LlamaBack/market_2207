40f70001efbc90c38fd2199e15fe07b5e716f11a
Jake Kim <imjakekim@gmail.com>
2022-08-08 08:10:27 -0700

    item spec creation

---
+++ spec/item_spec.rb
@@ -0,0 +1,12 @@
+require "./lib/item"
+
+RSpec.describe Item do
+  let(:item1) {Item.new({name: 'Peach', price: "$0.75"})}
+  let(:item2) {Item.new({name: 'Tomato', price: "$0.50"})}
+
+  it 'exists and has readable attributes' do
+    expect(item1).to be_a(Item)
+    expect(item1.name).to eq("Peach")
+    expect(item2.price).to eq(.5)
+  end
+end
